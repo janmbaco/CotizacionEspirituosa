1e4b6660ab7324a00e1caee64a8de5274f23e195
janmbaco <janmbaco@gmail.com>
2016-12-09 23:19:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 //app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico'))); 
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
