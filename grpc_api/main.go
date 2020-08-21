ac153659e643fd9b86a8037e9b4e8ac58992fa38
janmbaco <janmbaco@gmail.com>
2020-08-21 08:34:56 +0200

    add grpc

--- grpc_api/main.go
+++ grpc_api/main.go
@@ -5,26 +5,28 @@ import (
 	"google.golang.org/grpc"
 	"os"
 
+	"github.com/janmbaco/CotizacionEspirituosa/grpc_api/domain/services"
+	"github.com/janmbaco/CotizacionEspirituosa/grpc_api/domain/services/grpc_services"
 	"github.com/janmbaco/go-infrastructure/config"
 	"github.com/janmbaco/go-infrastructure/server"
 )
 
-type Config struct{
-	version string `json:"version"`
-	port string `json:"port"`
+type Config struct {
+	Version string `json:"version"`
+	Port    string `json:"port"`
 }
 
-func main(){
+func main() {
 	var configfile = flag.String("config", os.Args[0]+".config", "Config File")
 	flag.Parse()
 	configHandler := config.NewFileConfigHandler(*configfile)
-	conf := &Config{ version: "0.0.0", port: "80"}
+	conf := &Config{Version: "0.0.0", Port: "80"}
 	configHandler.Load(conf)
 
-	server.NewListener(configHandler, func(serverSetter *server.ServerSetter){
+	server.NewListener(configHandler, func(serverSetter *server.ServerSetter) {
 		serverSetter.ServerType = server.GRpcSever
-		serverSetter.Addr = conf.port
-	}).SetProtobuf(func(grpcServer *grpc.Server){
-		grpcServer.RegisterService()
+		serverSetter.Addr = conf.Port
+	}).SetProtobuf(func(grpcServer *grpc.Server) {
+		grpc_services.RegisterStatusServiceServer(grpcServer, &services.StatusService{})
 	}).Start()
 }
